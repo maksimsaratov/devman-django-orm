f584c248bf5b5dc390a51937acf649ec07618e97
Maksim Saratov <msaratov@iponweb.net>
2020-12-13 22:50:48 +0100

    Lesson 1 - step 4

--- devman/main.py
+++ devman/main.py
@@ -10,13 +10,19 @@ from datacenter.models import Passcard, Visit
 
 if __name__ == "__main__":
     # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())
+    print('Шаг 1')
+    print('Количество пропусков:', Passcard.objects.count(), "\n")
 
-    passcards = Passcard.objects.all()
-    print('Карточки пропусков:', passcards, "\n")
+    print('Шаг 2')
+    print('Карточки пропусков:', Passcard.objects.all(), "\n")
 
-    passcard = passcards[0]
-    print('owner_name:', passcard.owner_name)
-    print('passcode:', passcard.passcode)
-    print('created_at:', passcard.created_at)
-    print('is_active:', passcard.is_active)
+    print('Шаг 3')
+    any_passcard = Passcard.objects.all()[0]
+    print('owner_name:', any_passcard.owner_name)
+    print('passcode:', any_passcard.passcode)
+    print('created_at:', any_passcard.created_at)
+    print('is_active:', any_passcard.is_active, "\n")
+
+    print('Шаг 4')
+    print('ВСего пропусков:', Passcard.objects.count())
+    print('Активных пропусков:', Passcard.objects.filter(is_active=True).count(), "\n")
